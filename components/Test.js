3d5e25a8ac062857401f9c91fdcb6c547115dc23
iansanwich <iansanwich@gmail.com>
2020-07-24 15:19:30 +0800

    Add absolute importing

---
+++ components/Test.js
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div>Test 2</div>;
+};
+
+export default Test;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Test from "components/Test";
 
 const Title = styled.h1`
   font-size: 50px;
@@ -6,5 +7,9 @@ const Title = styled.h1`
 `;
 
 export default function Home() {
-  return <Title>My page</Title>;
+  return (
+    <Title>
+      <Test></Test>
+    </Title>
+  );
 }
